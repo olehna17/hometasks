31e354ffb725eab4bb227700cdcd2cb92fb7ec0c
Oleh Nazarenko <olehna@neogames.com>
2021-06-16 10:32:10 +0300

    initional commit

---
+++ Day6-loop/ConsoleApp3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter dimensions of your square(rectangle) X(lines count) x Y(columns count)");
+            int X = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+
+
+            //Pryamougolnik s svobodnim viborom razmernosti
+
+            for (int a = 0; a < X; a++)
+            {
+                for (int b = 0; b < Y; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            // Pryamougolniy treugolnik
+            for (int c = 0; c < 10; c++)
+            {
+                for (int d = 0; d < c; d++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            //Ravnostoronniy
+
+            int i, j, k;
+            for (i = 1; i <= 10; i += 2)
+            {
+
+                for (k = 10; k > i; k -= 2)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+            }
+
+            //Uberite comment esli nuzhen romb
+
+            //for (i = 7; i >= 0; i -= 2)
+            //{
+
+            //    for (k = 10; k > i; k -= 2)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (j = 0; j < i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.Write("\n");
+            //}
+        Console.ReadKey();
+        }
+    }
+}
---
+++ Day6-loop/ConsoleApp5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter count of clients");
+            int c = int.Parse(Console.ReadLine());
+
+            //count of cars pre described in exercise 
+            int factorial = 1;
+
+            do
+            {
+                factorial *= c--;
+            }
+            while (c > 0);
+
+            Console.WriteLine($"Count of variants of way {factorial}");
+
+            Console.ReadKey();
+        }
+    }
+}
